7b1eded16368e1105544231e086a9a6f68df229f
Michael Ferguson <michaelferguson@acm.org>
2014-01-09 14:42:35 -0500

    Update licensing blurb.

--- src/main/json.h
+++ src/main/json.h
@@ -1,3 +1,25 @@
+/*
+  (*) 2013-2014 Michael Ferguson <michaelferguson@acm.org>
+
+    * This is a work of the United States Government and is not protected by
+      copyright in the United States.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  femto/src/main/json.h
+*/
+
 #include "index_types.h"
 
 #define MAX_JSON 16
