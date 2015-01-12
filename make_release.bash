baf5beeaa65af4b0e3948383b86a93bdf9a421d5
Michael Ferguson <michaelferguson@acm.org>
2015-01-12 16:53:24 -0500

    Fixing make release

--- make_release.bash
+++ make_release.bash
@@ -7,10 +7,14 @@ mkdir ../femto_release
 pushd ../femto_release
 git clone $gitrepo
 cd femto
-sh autogen.sh
-./configure
-#make distcheck does a VPATH/out-of-tree-build not working with RE2
-make dist
+# instead of using make dist, we use git archive.
+#sh autogen.sh
+#./configure
+##make distcheck does a VPATH/out-of-tree-build not working with RE2
+#make dist
+VERSION=`head -n 1 ChangeLog | cut -d ' ' -f 1`
+ARCHIVE_NAME="femto-$VERSION"
+git archive --prefix=$ARCHIVE_NAME HEAD | gzip > $ARCHIVE_NAME.tar.gz
 mkdir dist-build
 cd dist-build
 tar xvzf ../femto*.tar.gz
