a224f4bee81d317b80d5037ae368586f238fcc91
Michael Ferguson <michaelferguson@acm.org>
2015-01-12 16:20:22 -0500

    Include CMakeLists.txt in release, fix make release for cmake

--- make_release.bash
+++ make_release.bash
@@ -16,29 +16,36 @@ cd dist-build
 tar xvzf ../femto*.tar.gz
 cd femto-*
 mkdir build
+cd build
 cmake ..
 if [ $? -ne 0 ]; then
   echo "Cmake failed"
+  exit 1
 fi
 make
 if [ $? -ne 0 ]; then
   echo "Cmake make failed"
+  exit 1
 fi
 make check
 if [ $? -ne 0 ]; then
   echo "Cmake make check failed"
+  exit 1
 fi
 cd ..
 ./configure
 if [ $? -ne 0 ]; then
   echo "autotools configure failed"
+  exit 1
 fi
 make
 if [ $? -ne 0 ]; then
   echo "autotools make failed"
+  exit 1
 fi
 make check
 if [ $? -ne 0 ]; then
   echo "autotools make check failed"
+  exit 1
 fi
 popd
