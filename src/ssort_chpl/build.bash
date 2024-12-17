2195191e8db4716a29077cc7e8bfc7833228310a
Michael Ferguson <mppf@users.noreply.github.com>
2024-12-17 14:55:12 -0500

    add build.bash
    ---
    Signed-off-by: Michael Ferguson <mppf@users.noreply.github.com>

---
+++ src/ssort_chpl/build.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ask bash to please exit if any command fails
+# (as indicated by a nonzero exit code)
+set -e
+
+# ask bash to print commands being run
+set -o xtrace
+
+chpl --fast SuffixSimilarity.chpl
+chpl --fast SuffixSort.chpl
+chpl --fast FindUnique.chpl
+chpl --fast ExtractUniqueKmers.chpl
